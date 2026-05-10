b7e9a9da29bab92c7e286948259977599946d120
Salvador Pineda <chava.ap07@gmail.com>
2026-05-09 21:19:04 -0600

    feat: ceremony add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import Timer from './Timer';
 import novios from '../assets/MyS.jpg';
 import plantilla3 from '../assets/plantilla3.png';
+import plantilla4 from '../assets/plantilla4.png';
+import iglesia from '../assets/iglesia.avif';
 
 const Home = () => {
   return (
@@ -27,44 +29,78 @@ const Home = () => {
 
       <div className="relative w-full max-w-4xl mx-auto mt-5">
 
-  <img 
-    src={plantilla3}
-    alt="Plantilla"
-    className="block w-full h-auto min-h-112.5 rounded-lg object-cover"
-  />
+        <img
+          src={plantilla3}
+          alt="Plantilla"
+          className="block w-full h-auto min-h-112.5 rounded-lg object-cover"
+        />
 
-  <div className="absolute inset-0 flex flex-col justify-center items-center p-6 text-center">
-    
-    <div className="flex flex-col gap-y-5"> 
-      
-      <h1 className="text-(--color-secondary) italic tracking-widest font-bold">
-        Con la bendición de Dios y nuestros padres
-      </h1>
+        <div className="absolute inset-0 flex flex-col justify-center items-center p-6 text-center">
 
-      <div className="flex flex-col gap-y-6">
-        
-        <div className="space-y-1">
-          <h2 className="text-(--color-secondary) font-bold text-[13px] tracking-tighter">
-            Padres de la novia
-          </h2>
-          <p className="text-(--color-secondary) text-base leading-tight">
-            María Rosalba Morales Flores <br /> Gerardo Medina Fernandez
-          </p>
+          <div className="flex flex-col gap-y-5">
+
+            <h1 className="text-(--color-secondary) italic tracking-widest font-bold">
+              Con la bendición de Dios y nuestros padres
+            </h1>
+
+            <div className="flex flex-col gap-y-6">
+
+              <div className="space-y-1">
+                <h2 className="text-(--color-secondary) font-bold text-[13px] tracking-tighter">
+                  Padres de la novia
+                </h2>
+                <p className="text-(--color-secondary) text-base leading-tight">
+                  María Rosalba Morales Flores <br /> Gerardo Medina Fernandez
+                </p>
+              </div>
+
+              <div className="space-y-1">
+                <h2 className="text-(--color-secondary) font-bold text-[13px] tracking-tighter">
+                  Padres del novio
+                </h2>
+                <p className="text-(--color-secondary) text-base leading-tight">
+                  María del Carmen Arredondo García <br /> Elias Pineda Durán
+                </p>
+              </div>
+
+            </div>
+          </div>
         </div>
-
-        <div className="space-y-1">
-          <h2 className="text-(--color-secondary) font-bold text-[13px] tracking-tighter">
-            Padres del novio
-          </h2>
-          <p className="text-(--color-secondary) text-base leading-tight">
-            María del Carmen Arredondo García <br /> Elias Pineda Durán
-          </p>
-        </div>
-
       </div>
-    </div>
-  </div>
-</div>
+
+      <div className="text-center mt-5 flex justify-center flex-col items-center">
+        <h2 className="text-[#6f4e37] text2xl font-bold">¿Dónde?</h2>
+      </div>
+
+      <div className="relative w-full max-w-4xl mx-auto mt-5">
+        {/* Imagen de fondo (La Plantilla) con la altura extendida */}
+        <img
+          src={plantilla3}
+          alt="Plantilla"
+          className="block w-full h-auto min-h-90 rounded-lg object-cover"
+        />
+
+        <div className="absolute inset-0 flex flex-col items-center justify-center p-8 text-center">
+          <h2 className="text-(--color-secondary) text-xl font-serif italic tracking-widest mb-2">
+            Ceremonia Religiosa
+          </h2>
+           <div className="space-y-1">
+            <h3 className="text-(--color-secondary) text-lg font-serif italic">
+              Capilla San Andrés
+            </h3>
+            <p className="text-(--color-secondary) text-sm font-Ballet italic tracking-wide">
+              30 Octubre 2026
+            </p>
+          </div>
+
+          <img
+            src={iglesia}
+            alt="Iglesia"
+            className="max-w-[80%] max-h-[50%] h-auto rounded-lg shadow-xl border-4 border-white/20 my-4"
+          />
+
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,7 +1,6 @@
 const Location = () => {
     return (
         <div className="text-center mt-5 flex justify-center flex-col items-center">
-            <h2 className="text-[#7a662f] text2xl font-bold">¿Dónde?</h2>
             <a
                 href="https://www.google.com/maps/place/Salón+para+Eventos:+Hacienda+San+Andrés/@19.4635636,-99.2348898,17z/data=!3m1!4b1!4m6!3m5!1s0x85d202528245e7a5:0x4961183f6f00e4e6!8m2!3d19.4635636!4d-99.2323149!16s%2Fg%2F1tmxfxhg?entry=ttu&g_ep=EgoyMDI2MDUwMi4wIKXMDSoASAFQAw%3D%3D"
                 target="_blank"
